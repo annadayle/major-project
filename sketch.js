6927244f0bca426767be332449246257397325ed
annadayle <77638079+annadayle@users.noreply.github.com>
2021-02-10 14:08:13 -0600

    added projectile visuals (collison detection and array splicing not avalible yet) feb 10th

--- sketch.js
+++ sketch.js
@@ -1,21 +1,32 @@
 // final project oh man here we go boys
 
+let theProjectiles = [];
 let playerx;
 let playery;
 let squareSize = 50;
-let speed = 7.5;
+let playerdx = 7.5;
 
 function setup() {
   let myCanvas = createCanvas(windowWidth*0.8, windowHeight*0.8);
   myCanvas.position(windowWidth*0.1, windowHeight*0.1);
   playerx = width/2;
   playery = height/2;
+  window.setInterval(spawnProjectile, 1000);
 }
 
 function draw() {
   background(220);
   displayPlayer();
   movePlayer();
+  for (i=theProjectiles.length-1; i>=0; i--) {
+    if (theProjectiles[i].isDead) {
+      theProjectiles.splice(i, 1);
+    }
+    else {
+      theProjectiles[i].move();
+      theProjectiles[i].display();
+    }
+  }
 }
 
 function displayPlayer() {
@@ -25,11 +36,43 @@ function displayPlayer() {
 
 function movePlayer() {
   if (keyIsDown(LEFT_ARROW)) {
-    playerx -= speed;
+    playerx -= playerdx;
   }
   if (keyIsDown(RIGHT_ARROW)) {
-    playerx += speed;
+    playerx += playerdx;
   }
 }
 
+function spawnProjectile() {
+  let someProjectile = new Projectile();
+  theProjectiles.push(someProjectile);
+}
 
+class Projectile {
+  constructor () {
+    this.x = random(width);
+    this.y = height - 800;
+    this.dy = 5;
+    this.theColor = "red";
+    this.radius = 20;
+    this.isAlive = true;
+    this.isDead = false;
+  }
+  
+  move() {
+    if (this.y -= this.radius > 0) {
+      this.y += this.dy;
+    }
+    else if (this.isAlive) {
+      this.isAlive = false;
+      this.isDead = true;
+    }
+  }
+
+  display() {
+    if (this.isAlive) {
+      fill(this.theColor);
+      ellipse(this.x, this.y, this.radius*2, this.radius*2);
+    }
+  }
+}
